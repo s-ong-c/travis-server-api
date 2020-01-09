b294f1b37509d345e1ea95e282cf92c3b28fbf37
s-ong-c <somony9292@gmail.com>
2020-01-09 18:16:17 +0900

    5001 -> 3000

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from './app';
 
 import './env';
 
-const { PORT } = process.env;
-app.listen(PORT, () => {
-  console.log('FFM server is listening to port', PORT);
+//const { PORT } = process.env;
+app.listen(3000, () => {
+  console.log('FFM server is listening to port 3000');
 });
